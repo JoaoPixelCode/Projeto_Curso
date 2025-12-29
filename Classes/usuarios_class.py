b5de7ba93800cfb367bb63848796e07c44ebe8d8
JoaoPixelCode <joaoritter2006@gmail.com>
2025-12-29 01:19:58 -0300

    Feat(adicioado na class usuario a validação do email e senha)

--- Classes/usuarios_class.py
+++ Classes/usuarios_class.py
@@ -1,11 +1,38 @@
 import re
+import string
 
 class validador_usuario:
     @staticmethod
+    #Talvez eu deva tirar esse Validador, pos o banca já impede que sejam inseridos email vazios e invalidos
     def ValidadorEmail(email):
+        padrao = r'^[\w\.-]+@[\w\.-]+\.[\w]{2,}$'
         if not email:
             return False, f"Por favor insira o email"
-        if  '@' not in email:
+        if '@' not in email:
             return False, f"Por favor insira um email valido"
-        else:
-            return True,None
+        if not re.match(padrao, email):
+            return False, f"Por favor insira um email valido"
+        
+        return True,None
+    
+    #Validador de senha funciona e é necessario, ele verifica se a senha está vazia, contem menos de 8 caracteres, 
+    #se possui letras maisculas,minusculas, caracteres especiais e se contem numeros, respectivamente
+    #Foi feito assim para forçar senhas fortes de usuarios/funcionarios
+    
+    def ValidadorSenha(senha, senha2):
+            if not senha:
+                return False, f"Por favor insira a sua senha"
+            if len(senha) < 8:
+                return False, f"Por favor insira ao menos 8 caracteres em sua senha"  
+            if not any(x.isupper() for x in senha):
+                return False, f"Por favor insira uma letra maiscula"
+            if not any(x.islower() for x in senha):
+                return False, f"Por favor insira uma letra minuscula"
+            if not any(x.isdigit() for x in senha):
+                return False, f"Por favor insira ao menos um numero"
+            if not any(x in string.punctuation for x in senha):
+                return False, f"Por favor insira ao menos um caracter especial (@,#,$,etc)"
+            if senha != senha2:
+                return False, f"Senhas nao sao iguais"
+           
+            return True, None
--- Controller/user.py
+++ Controller/user.py
@@ -17,8 +17,10 @@ def criar():
     senha = request.form.get("senha") or request.args.get("senha")
     data_criacao = request.form.get("data_criacao") or request.args.get("data_criacao")
     ativo = request.form.get("ativo") or request.args.get("ativo")
-     
+    senha2 = request.form.get("senha2") or request.args.get("senha2")
+
     valido, erro = validador_usuario.ValidadorEmail(email)
+    valido, erro = validador_usuario.ValidadorSenha(senha, senha2)
     
     if not valido:
         return jsonify({'erro': erro}), 400
@@ -28,7 +30,7 @@ def criar():
     
     result = db.session.execute(sql, dados)
     db.session.commit()  
-    return f"Criado com sucesso {nome}, {email}, {telefone}, {senha}, {data_criacao} e {ativo}"
+    return f"Criado com sucesso {nome}, {email}, {telefone}, {senha}, {data_criacao} e {ativo}, e {senha2}"
 
 
 
