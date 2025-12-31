040af048e8cf1b4e7e77b8c457f0ade58df8c90d
JoaoPixelCode <joaoritter2006@gmail.com>
2025-12-30 22:45:13 -0300

    Feat(adicione matricula e sua geração, creie algo para validar o telefone. adicione o get all e one, e o delete)

--- Classes/usuarios_class.py
+++ Classes/usuarios_class.py
@@ -1,5 +1,8 @@
 import re
 import string
+from datetime import date
+import random
+
 
 class validador_usuario:
     @staticmethod
@@ -15,10 +18,20 @@ class validador_usuario:
         
         return True,None
     
+    #Chamei de matricula por falta de um nome melhor,ele gera uma matricula basica usando o ano de cadastro, e dois numeros
+    #aleatorios um de 3 digitos e outro de 2 digitos.
+    def gerarMatricula(matricula):
+        data_atual = date.today().year
+        dia = date.today().day
+        numero_aleatorio = random.randint(100,999)
+        numero_aleatorio2 = random.randint(0,99)
+        numero = (dia * 2 * numero_aleatorio) % 1000
+        matricula='{}{}{}'.format(data_atual,numero,numero_aleatorio2)
+        return f'{matricula}'
+
     #Validador de senha funciona e é necessario, ele verifica se a senha está vazia, contem menos de 8 caracteres, 
     #se possui letras maisculas,minusculas, caracteres especiais e se contem numeros, respectivamente
     #Foi feito assim para forçar senhas fortes de usuarios/funcionarios
-    
     def ValidadorSenha(senha, senha2):
             if not senha:
                 return False, f"Por favor insira a sua senha"
@@ -36,3 +49,12 @@ class validador_usuario:
                 return False, f"Senhas nao sao iguais"
            
             return True, None
+    def ValidadorTelefone(telefone):
+        if not telefone:
+            return False, f"Por favor insira um telefone para contato"
+        if len(telefone) < 8:
+            return False,f"Insira um numero valido"
+        if len(telefone) > 10:
+            return False,f"Insiria um numero valido"
+        
+        return True, None
--- Controller/user.py
+++ Controller/user.py
@@ -1,6 +1,7 @@
 from flask import Flask,Blueprint, request, jsonify
 from sqlalchemy import text
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 from Banco.database import db
 from Classes.usuarios_class import validador_usuario
@@ -18,26 +19,87 @@ def criar():
     data_criacao = request.form.get("data_criacao") or request.args.get("data_criacao")
     ativo = request.form.get("ativo") or request.args.get("ativo")
     senha2 = request.form.get("senha2") or request.args.get("senha2")
+    matricula = request.form.get("matricula") or request.args.get("matricula")
 
     valido, erro = validador_usuario.ValidadorEmail(email)
     valido, erro = validador_usuario.ValidadorSenha(senha, senha2)
+    valido, erro = validador_usuario.ValidadorTelefone(telefone)
     
     if not valido:
         return jsonify({'erro': erro}), 400
     # SQL
-    sql = text("INSERT INTO users (nome, email, telefone, senha, data_criacao,ativo) VALUES (:nome, :email, :telefone, :senha, :data_criacao, :ativo)")
-    dados = {"nome": nome, "email": email, "telefone": telefone, "senha": senha, "data_criacao": data_criacao, "ativo": ativo}
+    ativo=(True)
+    data_criacao = date.today()
+    matricula = validador_usuario.gerarMatricula(matricula)
+    sql = text("INSERT INTO users (nome, email, telefone, senha, data_criacao,ativo, matricula) VALUES (:nome, :email, :telefone, :senha, :data_criacao, :ativo, :matricula)")
+    dados = {"nome": nome, "email": email, "telefone": telefone, "senha": senha, "data_criacao": data_criacao, "ativo": ativo, "matricula": matricula}
     
     result = db.session.execute(sql, dados)
     db.session.commit()  
-    return f"Criado com sucesso {nome}, {email}, {telefone}, {senha}, {data_criacao} e {ativo}, e {senha2}"
+    return f"Criado com sucesso {nome}, {email}, {telefone}, {senha}, {data_criacao} e {ativo}, e {senha2}, {matricula}"
+
+@usuario.route("/all")
+def get_ALL(): 
+    sql_query = text("SELECT * FROM users")
+    try:
+        # result sem dados
+        result = db.session.execute(sql_query)
+        
+        relatorio = result.mappings().all()
+        json = [dict(row) for row in relatorio]  # Gambi pq cada linha é um objeto
+        
+        return json
+    except Exception as e:
+        return str(e)
+    
 
 
+@usuario.route("/<matricula>", methods=["POST"]) 
+def get_one(matricula): 
+    sql_query = text("SELECT * FROM users where matricula = :matricula ")
+    dados = {"matricula": matricula}
 
+    try:
+        result = db.session.execute(sql_query,dados)
+        # Mapear todas as colunas para a linha
+        linha = result.mappings().all()[0]
+        print(linha)
+        return dict(linha)
+    except Exception as e:
+        return str(e)
+    
+@usuario.route("/<id>", methods=["Put"])
+def desativar_usuario(id):
+    ativo = False
 
-#   "id": 1,
-#    "nome": "João Silva",
-#   "email": "joao@empresa.com",
-#   "telefone": "51999999999",
-#   "senha": "senha",
-#   "data_criacao": "2025-01-15"
+    sql = text("UPDATE users set ativo = :ativo where id = :id")
+    dados = {"ativo": ativo, "id": id}
+
+    result = db.session.execute(sql, dados)
+    
+    linhas_afetadas = result.rowcount
+    
+    if linhas_afetadas == 1:
+        db.session.commit()
+        return f"Cliente com o {id} desativado"
+    else:
+        db.session.rollback()
+        return f"Só deus na causa"  
+    
+@usuario.route("/reativar/<id>", methods=["PUT"])
+def reativar_usuario(id):
+    ativo = True
+
+    sql = text("UPDATE users SET ativo = :ativo WHERE id = :id")
+    dados = {"ativo": ativo, "id": id}
+
+    result = db.session.execute(sql, dados)
+
+    linhas_afetadas = result.rowcount
+
+    if linhas_afetadas == 1:
+        db.session.commit()
+        return f"Cliente com o id {id} foi reativado com sucesso!"
+    else:
+        db.session.rollback()
+        return f"Nenhum usuário encontrado com id {id}, reativação não efetuada."
